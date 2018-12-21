8f2289f144290f8e64b990b653075f7574292212
CharlotteSophia7th <league@WTS14.attlocal.net>
2018-12-20 16:57:01 -0800

    robotcolorchooser and crazycatlady

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -3,25 +3,46 @@
 
 package elseif;
 
-import javax.swing.JOptionPane;
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot hi = new Robot();
+		// 3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 4; i++) {
 
-        //6. If the user doesn’t enter anything, choose a random color
+			String bye = JOptionPane.showInputDialog("What color would you like the robot to draw?");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (bye.equalsIgnoreCase("black")) {
+				hi.setPenColor(Color.black);
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
+			else {
+				hi.setRandomPenColor();
+			}
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
+			// 4. Set the pen width to 10
+			hi.setPenWidth(10);
+
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			hi.penDown();
+			hi.setSpeed(50);
+			hi.move(200);
+			hi.turn(90);
+			hi.move(200);
+			hi.turn(90);
+			hi.move(200);
+			hi.turn(90);
+			hi.move(200);
+		}
 
 	}
 }
