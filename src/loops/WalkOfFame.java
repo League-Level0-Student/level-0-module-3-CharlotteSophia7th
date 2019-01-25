daf155ccfd23bea0bef612fd70ddff5562b4938e
CharlotteSophia7th <league@WTS14.attlocal.net>
2019-01-24 16:54:34 -0800

    walkoffame and fizzbuzz

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,17 +9,23 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
 
+		rob.setAngle(-90);
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		for (int i = 0; i < 6; i++) {
+			System.out.println(i);
+			rob.turn(144);
+			rob.setSpeed(60);
+			rob.move(500);
+		}
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 * Make the robot draw a line of stars like this: http://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
